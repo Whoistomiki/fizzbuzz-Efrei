0bc6ad31d8f5820aecfd3a1622c9586b9c317c42
Whoistomiki <thomasyu@hotmail.fr>
2025-02-06 09:50:27 +0100

    Fix files function and rename

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,18 +1,11 @@
 import pytest
 from io import StringIO
 from unittest.mock import patch
+from fizzbuzz import fizzbuzz
 
 def test_fizzbuzz():
     with patch('sys.stdout', new_callable=StringIO) as fizzbuzz_test:
-        for i in range(1, 101):
-            if i % 3 == 0 and i % 5 == 0:
-                print("FizzBuzz")
-            elif i % 3 == 0:
-                print("Fizz")
-            elif i % 5 == 0:
-                print("Buzz")
-            else:
-                print(i)
+        fizzbuzz()
 
         output = fizzbuzz_test.getvalue()
 
