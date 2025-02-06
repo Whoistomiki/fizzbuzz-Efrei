c540745ed647c700960d0c16b256c6bec703c1d3
Whoistomiki <thomasyu@hotmail.fr>
2025-02-06 10:33:56 +0100

    Fix multiple tests and file fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,16 @@
-def fizzbuzz():
-    for i in range(1, 101):
+def fizzbuzz(n):
+    result = []
+    for i in range(1, n):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            result.append("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz")
+            result.append("Fizz")
         elif i % 5 == 0:
-            print("Buzz")
+            result.append("Buzz")
         else:
-            print(i)
+            result.append(str(i))
+    return result
 
 if __name__ == "__main__":
-    fizzbuzz()
+    for res in fizzbuzz(101):
+        print(res)
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,23 +1,13 @@
 import pytest
-from io import StringIO
-from unittest.mock import patch
 from fizzbuzz import fizzbuzz
 
+def test_fizz():
+    assert fizzbuzz(4) == ['1', '2', 'Fizz']
+
+def test_buzz():
+    assert fizzbuzz(6) == ['1', '2', 'Fizz', '4', 'Buzz']
+
 def test_fizzbuzz():
-    with patch('sys.stdout', new_callable=StringIO) as fizzbuzz_test:
-        fizzbuzz()
+    assert fizzbuzz(16) == ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']
 
-        output = fizzbuzz_test.getvalue()
-
-        expected_output = ""
-        for i in range(1, 101):
-            if i % 3 == 0 and i % 5 == 0:
-                expected_output += "FizzBuzz\n"
-            elif i % 3 == 0:
-                expected_output += "Fizz\n"
-            elif i % 5 == 0:
-                expected_output += "Buzz\n"
-            else:
-                expected_output += f"{i}\n"
-
-        assert output == expected_output
+test_fizzbuzz()
