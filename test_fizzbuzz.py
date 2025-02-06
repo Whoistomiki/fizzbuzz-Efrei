81befde4a35e99f06654ac04da05c395936f5a01
Whoistomiki <thomasyu@hotmail.fr>
2025-02-06 11:48:28 +0100

    Adding a new test

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -9,5 +9,6 @@ def test_buzz():
 
 def test_fizzbuzz():
     assert fizzbuzz(16) == ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']
+    assert fizzbuzz(31) == ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz', '16', '17', 'Fizz', '19', 'Buzz', 'Fizz', '22', '23', 'Fizz', 'Buzz', '26', 'Fizz', '28', '29', 'FizzBuzz']
 
 test_fizzbuzz()
