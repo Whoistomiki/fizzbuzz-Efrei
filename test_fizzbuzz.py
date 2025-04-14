46c5d2d352cecb444838998d6c9225df437211a0
Eric Siber <eric@agiledev.tech>
2025-04-14 15:33:59 +0200

    Make test fail

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,7 +2,7 @@ import pytest
 from fizzbuzz import fizzbuzz
 
 def test_fizz():
-    assert fizzbuzz(4) == ['1', '2', 'Fizz']
+    assert fizzbuzz(4) == ['1', '2', 'Buzz']
 
 def test_buzz():
     assert fizzbuzz(6) == ['1', '2', 'Fizz', '4', 'Buzz']
